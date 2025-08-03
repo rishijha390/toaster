f121f4f4f1640f483b6ff3e6b7c58a7f8dc4c94a
rishijha390 <rishijha390@gmail.com>
2025-08-03 23:11:13 +0530

    Updated toaster notification feature

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 function createToaster(config){
-  return function () {
+  return function (str) {
   let div =  document.createElement("div");
   div.textContent = str;
+  div.className = `inline-block ${config.theme === "dark" ? "bg-gray-800 text-white" : "bg-gray-100 text-black"}
+  px-6 py-3 rounded shadow-lg pointer-events-none`;
 
-  
+   document.querySelector(".parent").appendChild(div);
+
+   if(config.positionX !== "left" || positionY !== "top") {
+    // document.querySelector(".parent").classList.add("fixed");
+    document.querySelector(".parent").className += 
+    ` ${config.positionX === "right" ? " right-5" : "  left-5"} ${config.positionY === "bottom" ? " bottom-5" : "top-5"}`;
+   }
+   setTimeout(() => {
+    document.querySelector(".parent").removeChild(div);
+   }, config.duration * 1000);
   };
 }
 
-createToaster({
+ let toaster = createToaster({
     positionX: "right",
-    positionY:"top",
-    theme: "dark",
+    positionY:"bottom",
+    theme: "light",
     duration: 3,
 });
-createToaster("Download Done");
+toaster("Download Done");
+setTimeout(() => {
+  toaster("Rishi accepted your request");
+},2000);
